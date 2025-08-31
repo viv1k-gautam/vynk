ed1670bb05642ea30398c11152937ec75ae4925e
viv1k-gautam <vks28236@gmail.com>
2025-09-01 00:54:15 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -21,6 +21,8 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: false }));
 
+app.use(cors());
+
 app.use('/',require('./routes/authRoutes'));
 
 app.use(passport.initialize());
