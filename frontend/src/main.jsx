37c88a22ae956128fe6aedaa151437b3e88eb864
viv1k-gautam <vks28236@gmail.com>
2025-09-01 01:48:33 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,11 +5,10 @@ import './index.css'
 import App from './App.jsx'
 import{GoogleOAuthProvider} from '@react-oauth/google'
 import { BrowserRouter } from 'react-router-dom';
-import axios from "axios";
 
 
-axios.defaults.baseURL = "https://vynk-backend.onrender.com"; 
-axios.defaults.withCredentials = true;
+
+
 
 createRoot(document.getElementById('root')).render(
   //<StrictMode>
