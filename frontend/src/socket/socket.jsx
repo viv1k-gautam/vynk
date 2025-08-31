29828f3da2bc9364c4ec9c2449c1400604af0e77
viv1k-gautam <vks28236@gmail.com>
2025-08-31 22:27:52 +0530

    Update socket.jsx

--- frontend/src/socket/socket.jsx
+++ frontend/src/socket/socket.jsx
@@ -1,3 +1,3 @@
 import { io } from "socket.io-client";
-const socket = io("http://localhost:8000"); // apna backend ka port
+const socket = io("https://vynk-backend.onrender.com"); // apna backend ka port
 export default socket;
