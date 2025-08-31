330b2a70e9fa34c9f9e53dcd46fa323760ba99dd
viv1k-gautam <vks28236@gmail.com>
2025-09-01 01:25:06 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,10 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: false }));
 
-app.use(cors());
+app.use(cors({
+  origin: "https://vynk-frontend.onrender.com",
+  credentials: true
+}));
 
 app.use('/',require('./routes/authRoutes'));
 
