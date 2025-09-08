ececbcaceb0d75866a07b555a170e885ef87fc00
viv1k-gautam <vks28236@gmail.com>
2025-09-08 20:01:51 +0530

    Update Login.jsx add comma

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -20,7 +20,7 @@ function Login() {
       const res = await axios.post(`${import.meta.env.VITE_API_URL}/login`, {
         email, 
         password
-      }
+      },
                    { withCredentials: true }                         
     );
       if (res.data.error){
