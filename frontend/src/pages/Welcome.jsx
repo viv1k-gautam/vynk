4a8a9f0d3873702d9a29d4c61d441adf8d89888d
viv1k-gautam <vks28236@gmail.com>
2026-01-06 23:26:04 +0530

    Add Link import to Welcome.jsx

--- frontend/src/pages/Welcome.jsx
+++ frontend/src/pages/Welcome.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 import { useContext } from 'react'
 import { UserContext } from '../../context/userContext'
 import LogoutButton from '../components/LogoutButton'
